57711f75c92663c795bac0723889dbba75eb5406
= <cali@calicastle.com>
2016-01-30 22:58:37 +0800

    Update home sidebar and styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,9 @@
                 {{-- Banner Start --}}
                 <div class="banner">
                     <ul class="banner-list">
-                        <li class="active"><a href=""><img src="/images/banner1.jpg" alt=""></a></li>
-                        <li><a href=""><img src="/images/banner2.jpg" alt=""></a></li>
-                        <li><a href=""><img src="/images/banner3.jpg" alt=""></a></li>
+                        <li class="active"><a href=""><img src="images/banner1.jpg" alt=""></a></li>
+                        <li><a href=""><img src="images/banner2.jpg" alt=""></a></li>
+                        <li><a href=""><img src="images/banner3.jpg" alt=""></a></li>
                     </ul>
                     <div class="banner-dots">
                         <div class="banner-nav">
@@ -27,8 +27,8 @@
                     </div>
 
                     <div class="panel-body">
-                        <ul>
-                            <li><p>信息</p></li>
+                        <ul class="home-list">
+                            <li><p><span>[供]</span>信息信息信息信息<span class="time">2016-01-31</span></p></li>
                             <li><p>信息</p></li>
                             <li><p>信息</p></li>
                             <li><p>信息</p></li>
@@ -49,7 +49,27 @@
                             <li><p>信息</p></li>
                         </ul>
                     </div>
+					<nav class="home-pagination">
+					  <ul class="pagination">
+					    <li>
+					      <a href="#" aria-label="Previous">
+					        <span aria-hidden="true">&laquo;</span>
+					      </a>
+					    </li>
+					    <li><a href="#">1</a></li>
+					    <li><a href="#">2</a></li>
+					    <li><a href="#">3</a></li>
+					    <li><a href="#">4</a></li>
+					    <li><a href="#">5</a></li>
+					    <li>
+					      <a href="#" aria-label="Next">
+					        <span aria-hidden="true">&raquo;</span>
+					      </a>
+					    </li>
+					  </ul>
+					</nav>
                 </div>
+
             </div>
             <div class="col-md-4">
                 <div class="create-panel panel panel-success">
@@ -59,7 +79,13 @@
                 </div>
                 <div class="panel panel-success">
                     <div class="panel-heading">Sidebar</div>
-                    <div class="panel-body">Test</div>
+                    <div class="panel-body">
+						<ul class="home-cooperation">
+							<li><span class="time">1 小时前</span><a href="">红富士苹果膜袋/纸袋红富士苹果：0.6-1.2元/斤</a></li>
+							<li><span class="time">1 小时前</span><a href="">红富士苹果膜袋/纸袋红富士苹果：0.6-1.2元/斤</a></li>
+							<li><span class="time">1 小时前</span><a href="">红富士苹果膜袋/纸袋红富士苹果：0.6-1.2元/斤</a></li>
+						</ul>
+                    </div>
                 </div>
                 <div class="panel panel-success">
                     <div class="panel-heading">Sidebar</div>
